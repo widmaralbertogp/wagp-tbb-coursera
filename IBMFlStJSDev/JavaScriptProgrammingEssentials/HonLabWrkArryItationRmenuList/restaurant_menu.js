5984b503ae7d6e82f183b9cb4e70d2fd60c75a38
Widmar Alberto Gómez Pérez <widmar.alberto.gp@gmail.com>
2025-08-24 23:18:41 -0500

    Commit 24Aug25

---
+++ IBMFlStJSDev/JavaScriptProgrammingEssentials/HonLabWrkArryItationRmenuList/restaurant_menu.js
@@ -0,0 +1,20 @@
+const breakfastMenu = ['Pancakes- $12', 'Eggs Benedict -$22.99', 'Oatmeal -$21.99', 'Frittata -$15'];
+const mainCourseMenu = ['Steak- $22', 'Pasta', 'Burger', 'Salmon'];
+const dessertMenu = ['Cake- $17', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+
+const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
+document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
+
+
+let mainCourseItem = '';
+mainCourseMenu.forEach((item, index) => {
+mainCourseItem += `<p>Item ${index + 1}: ${item}</p>`;});
+document.getElementById('maincourseMenuItems').innerHTML = mainCourseItem;
+
+
+let dessertItem = '';
+for (let i = 0; i < dessertMenu.length; i++) 
+{
+    dessertItem += `<p>Item ${i + 1}: ${dessertMenu[i]}</p>`;
+}
+document.getElementById('dessertMenuItems').innerHTML = dessertItem;
