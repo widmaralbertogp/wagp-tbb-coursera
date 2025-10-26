5202791bd8304793a60cae41d4540741e44e6cab
Widmar Alberto Gómez Pérez <widmar.alberto.gp@gmail.com>
2025-10-26 03:54:09 -0500

    Commit 26Oct25 03:54am

---
+++ IBMFlStJSDev/JavaScriptProgrammingEssentials/Module 4/finalProject/imagos.sh
@@ -0,0 +1,19 @@
+# Australia
+curl -L -o images/sydney.jpg "https://images.unsplash.com/photo-1506976785307-8732e854ad77?crop=entropy&cs=tinysrgb&fit=crop&w=300&h=200&auto=format&fm=jpg&q=80"
+curl -L -o images/melbourne.jpg "https://images.unsplash.com/photo-1578632767115-351597cf2477?crop=entropy&cs=tinysrgb&fit=crop&w=300&h=200&auto=format&fm=jpg&q=80"
+
+# Japan
+curl -L -o images/tokyo.jpg "https://images.unsplash.com/photo-1526483360412-f4dbaf036963?crop=entropy&cs=tinysrgb&fit=crop&w=300&h=200&auto=format&fm=jpg&q=80"
+curl -L -o images/kyoto.jpg "https://images.unsplash.com/photo-1553413077-190dd305871c?crop=entropy&cs=tinysrgb&fit=crop&w=300&h=200&auto=format&fm=jpg&q=80"
+
+# Brazil
+curl -L -o images/rio.jpg "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?crop=entropy&cs=tinysrgb&fit=crop&w=300&h=200&auto=format&fm=jpg&q=80"
+curl -L -o images/sao_paulo.jpg "https://images.unsplash.com/photo-1589394811322-58e83b4e401f?crop=entropy&cs=tinysrgb&fit=crop&w=300&h=200&auto=format&fm=jpg&q=80"
+
+# Temples
+curl -L -o images/angkor_wat.jpg "https://images.unsplash.com/photo-1584345604477-7cf1aa2f9778?crop=entropy&cs=tinysrgb&fit=crop&w=300&h=200&auto=format&fm=jpg&q=80"
+curl -L -o images/taj_mahal.jpg "https://images.unsplash.com/photo-1584982481264-3894c01f2a9f?crop=entropy&cs=tinysrgb&fit=crop&w=300&h=200&auto=format&fm=jpg&q=80"
+
+# Beaches
+curl -L -o images/bora_bora.jpg "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?crop=entropy&cs=tinysrgb&fit=crop&w=300&h=200&auto=format&fm=jpg&q=80"
+curl -L -o images/copacabana.jpg "https://images.unsplash.com/photo-1509822929063-6b6cfc9b42f2?crop=entropy&cs=tinysrgb&fit=crop&w=300&h=200&auto=format&fm=jpg&q=80"
